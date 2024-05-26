4374e52ce6e1e421473d318447917db4556419a4
umarshakoor <umarshakoor70@outlook.com>
2024-05-27 01:49:42 +0500

    Sunday

--- file-1/vite-project/src/App.jsx
+++ file-1/vite-project/src/App.jsx
@@ -1,13 +1,13 @@
 import EffectHook from "./components/EffectHook";
+import WidthHeight from "./components/EffectState";
 import RenderText from "./components/RenderText";
 import Greeting from "./components/StateHook";
+import UseEffectHook from "./components/UseEffectHook";
 
 function App() {
   return (
     <>
-      <RenderText />
-      <EffectHook />
-      <Greeting />
+      <UseEffectHook />
     </>
   );
 }
---
+++ file-1/vite-project/src/components/UseEffectHook.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+
+function UseEffectHook() {
+
+    const [count, setCount] = useState(0);
+    const [show, setShow] = useState(false);
+    const [products, setProducts] = useState([]);
+
+    async function fetchAllProduct() {
+        try {
+            const response = await fetch('https://dummyjson.com/products')
+            const result = await response.json();
+
+            if (result && result.products) setProducts(result.products);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    useEffect(() => {
+        fetchAllProduct();
+    }, [])
+    useEffect(() => {
+        if (count === 5) setShow(true);
+    }, [count])
+    return (
+        <>
+            <h1>Use Effect Hook</h1>
+            <p>Count is {count}</p>
+            {show ? <h2>Hello World</h2> : null}
+            <button onClick={() => setCount(count + 1)}>Increase Count</button>
+            <button onClick={() => setCount(count - 1)}>Decrease Count</button>
+            <ul>
+                {
+                    products && products.length > 0 ? products.map(item => <li key={item.id}>{item.title}</li>) : null
+                }
+            </ul>
+        </>
+    )
+}
+
+export default UseEffectHook;
