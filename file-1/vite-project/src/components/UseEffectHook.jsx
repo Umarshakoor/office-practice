96812c6e2a4503f80607d35b1e86d8b7c8274e38
umarshakoor <umarshakoor70@outlook.com>
2024-05-27 06:00:14 -0700

    27/05 office

--- file-1/vite-project/src/components/UseEffectHook.jsx
+++ file-1/vite-project/src/components/UseEffectHook.jsx
@@ -1,42 +1,43 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function UseEffectHook() {
+  const [count, setCount] = useState(0);
+  const [show, setShow] = useState(false);
+  const [products, setProducts] = useState([]);
 
-    const [count, setCount] = useState(0);
-    const [show, setShow] = useState(false);
-    const [products, setProducts] = useState([]);
+  async function fetchAllProduct() {
+    try {
+      const response = await fetch(
+        "https://dummyjson.com/products/category/smartphones"
+      );
+      const result = await response.json();
 
-    async function fetchAllProduct() {
-        try {
-            const response = await fetch('https://dummyjson.com/products')
-            const result = await response.json();
-
-            if (result && result.products) setProducts(result.products);
-        } catch (e) {
-            console.error(e);
-        }
+      if (result && result.products) setProducts(result.products);
+    } catch (e) {
+      console.error(e);
     }
+  }
 
-    useEffect(() => {
-        fetchAllProduct();
-    }, [])
-    useEffect(() => {
-        if (count === 5) setShow(true);
-    }, [count])
-    return (
-        <>
-            <h1>Use Effect Hook</h1>
-            <p>Count is {count}</p>
-            {show ? <h2>Hello World</h2> : null}
-            <button onClick={() => setCount(count + 1)}>Increase Count</button>
-            <button onClick={() => setCount(count - 1)}>Decrease Count</button>
-            <ul>
-                {
-                    products && products.length > 0 ? products.map(item => <li key={item.id}>{item.title}</li>) : null
-                }
-            </ul>
-        </>
-    )
+  useEffect(() => {
+    fetchAllProduct();
+  }, []);
+  useEffect(() => {
+    if (count === 5) setShow(true);
+  }, [count]);
+  return (
+    <>
+      <h1>Use Effect Hook</h1>
+      <p>Count is {count}</p>
+      {show ? <h2>Hello World</h2> : null}
+      <button onClick={() => setCount(count + 1)}>Increase Count</button>
+      <button onClick={() => setCount(count - 1)}>Decrease Count</button>
+      <ul>
+        {products && products.length > 0
+          ? products.map((item) => <li key={item.id}>{item.title}</li>)
+          : null}
+      </ul>
+    </>
+  );
 }
 
 export default UseEffectHook;
