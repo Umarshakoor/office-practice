11f2e0f6328eee80c3d6b9d3b2fc2945967ed12e
umarshakoor <umarshakoor70@outlook.com>
2024-07-26 01:26:16 +0500

    home task @Umarshakoor

--- file-7/portfolio/src/App.jsx
+++ file-7/portfolio/src/App.jsx
@@ -4,14 +4,16 @@ import "./App.css";
 // import AccordionTask from "./components/AccordionTask";
 import PortfolioLandingPage from "./components/Portfolio";
 import Project from "./components/Project.jsx";
+import ProductDetails from "./components/ProductDetails.jsx";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/portfolio" element={<PortfolioLandingPage />} />
+          <Route path="/" element={<PortfolioLandingPage />} />
           <Route path="/project" element={<Project />} />
+          <Route path="/product" element={<ProductDetails />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ file-7/portfolio/src/components/ProductDetails.jsx
@@ -0,0 +1,205 @@
+import React from "react";
+import "./ProductDetails.css";
+import brick from "../assets/brick.svg";
+import cement from "../assets/cement.svg";
+import pipe from "../assets/pipe.svg";
+import sand from "../assets/sand.svg";
+import steel from "../assets/steel.svg";
+import wood from "../assets/wood.svg";
+import dots from "../assets/dots.svg";
+function ProductDetails() {
+  return (
+    <div>
+      <header className="dashboard-header">
+        <nav className="dashboard-nav">
+          <ul className="dashboard-ul">
+            <li className="option-li">Home</li>
+            <li className="option-li">
+              Deadlines <span className="rotate-arrow">^</span>
+            </li>
+            <li className="option-li" style={{ color: "rgb(15, 97, 174)" }}>
+              Project <span className="rotate-arrow">^</span>
+            </li>
+            <li className="option-li">Request</li>
+            <li className="option-li">
+              Activities <div className="rotate-arrow">^</div>
+            </li>
+            <li className="option-li">
+              Material <span className="rotate-arrow">^</span>
+            </li>
+            <li className="option-li">
+              Payment <span className="rotate-arrow">^</span>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <div className="two-columns">
+          <div className="first-column">
+            <div className="first-column-heading">
+              <h4 className="products-heading">Products</h4>
+            </div>
+            <div className="first-column-content">
+              <div className="products-card">
+                <img className="first-column-img" src={sand} alt="sand" />
+                <div className="product-quality-dots">
+                  <div className="product-quality-div">
+                    <h4 className="product-name">Sand</h4>
+                    <h4 className="product-quality">Premium Quality</h4>
+                  </div>
+                  <div className="three-dots-div">
+                    <img
+                      className="product-dots"
+                      src={dots}
+                      alt="more option"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="first-column-divider"></div>
+              <div className="products-card">
+                <img className="first-column-img" src={cement} alt="sand" />
+                <div className="product-quality-dots">
+                  <div className="product-quality-div">
+                    <h4 className="product-name">Cement</h4>
+                    <h4 className="product-quality">Premium Quality</h4>
+                  </div>
+                  <div className="three-dots-div">
+                    <img
+                      className="product-dots"
+                      src={dots}
+                      alt="more option"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="first-column-divider"></div>
+              <div className="products-card">
+                <img className="first-column-img" src={pipe} alt="sand" />
+                <div className="product-quality-dots">
+                  <div className="product-quality-div">
+                    <h4 className="product-name">Pipe</h4>
+                    <h4 className="product-quality">Premium Quality</h4>
+                  </div>
+                  <div className="three-dots-div">
+                    <img
+                      className="product-dots"
+                      src={dots}
+                      alt="more option"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="first-column-divider"></div>
+              <div className="products-card">
+                <img className="first-column-img" src={steel} alt="sand" />
+                <div className="product-quality-dots">
+                  <div className="product-quality-div">
+                    <h4 className="product-name">Steel</h4>
+                    <h4 className="product-quality">Premium Quality</h4>
+                  </div>
+                  <div className="three-dots-div">
+                    <img
+                      className="product-dots"
+                      src={dots}
+                      alt="more option"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="first-column-divider"></div>
+              <div className="products-card">
+                <img className="first-column-img" src={brick} alt="sand" />
+                <div className="product-quality-dots">
+                  <div className="product-quality-div">
+                    <h4 className="product-name">Brick</h4>
+                    <h4 className="product-quality">Premium Quality</h4>
+                  </div>
+                  <div className="three-dots-div">
+                    <img
+                      className="product-dots"
+                      src={dots}
+                      alt="more option"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="first-column-divider"></div>
+              <div className="products-card">
+                <img className="first-column-img" src={wood} alt="sand" />
+                <div className="product-quality-dots">
+                  <div className="product-quality-div">
+                    <h4 className="product-name">Wood</h4>
+                    <h4 className="product-quality">Premium Quality</h4>
+                  </div>
+                  <div className="three-dots-div">
+                    <img
+                      className="product-dots"
+                      src={dots}
+                      alt="more option"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="first-column-divider"></div>
+              <div className="products-card">
+                <img className="first-column-img" src={sand} alt="sand" />
+                <div className="product-quality-dots">
+                  <div className="product-quality-div">
+                    <h4 className="product-name">Sand</h4>
+                    <h4 className="product-quality">Premium Quality</h4>
+                  </div>
+                  <div className="three-dots-div">
+                    <img
+                      className="product-dots"
+                      src={dots}
+                      alt="more option"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="first-column-divider"></div>
+              <div className="products-card">
+                <img className="first-column-img" src={cement} alt="sand" />
+                <div className="product-quality-dots">
+                  <div className="product-quality-div">
+                    <h4 className="product-name">Cement</h4>
+                    <h4 className="product-quality">Premium Quality</h4>
+                  </div>
+                  <div className="three-dots-div">
+                    <img
+                      className="product-dots"
+                      src={dots}
+                      alt="more option"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="first-column-divider"></div>
+            </div>
+          </div>
+          <div className="second-column">
+            <div className="second-column-heading">
+              <h4>Product details</h4>
+            </div>
+            <div>
+              <img src={brick} alt="" />
+            </div>
+            <div className="label-input">
+              <label className="label-text" htmlFor="">
+                Phase Name
+              </label>
+              <input
+                className="form-input"
+                type="text"
+                placeholder="IDK Phase"
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default ProductDetails;
