3a162ec7a81c4c203d57806eb127aca585bfad06
umarshakoor <umarshakoor70@outlook.com>
2024-09-19 10:14:55 +0500

    creating a new component for navbar and deleting unstructured files

---
+++ file-4/react-context/src/context/AppState.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import AppContext from "./AppContext";
+
+const AppState = ({ children }) => {
+  const [state, setState] = useState({});
+
+  return (
+    <AppContext.Provider value={{ state, setState }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppState;
